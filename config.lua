3b263edd9abd86d5424665eb7cc98604757c5b70
rahimhamid <78023684+rahimhamid@users.noreply.github.com>
2021-09-20 18:57:21 +0100

    Update config.lua

--- config.lua
+++ config.lua
@@ -3,13 +3,12 @@ Config = Config or {}
 Config.CompanyPrice = 25000
 
 Config.Cityhall = {
-    coords = vector3(-265.0, -963.6, 31.2)
+    coords = vector3(-552.0,-191.68,38.22)
 }
 
 Config.DriverTest = {
-    coords = vector3(-549.86, -191.75, 38.22)
+    coords = vector3(-549.86,-191.75,38.22)
 }
 
 Config.DrivingSchool = {
-    coords = vector3(232.5, 368.7, 105.94)
-}
+    coords = vector3(232.5,368.7,105.94)
